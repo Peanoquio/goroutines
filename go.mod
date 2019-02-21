8e5c7e8e6a498989d41700a678ae96d7c9083655
Oliver <oliver@circles.asia>
2019-02-21 14:32:58 +0800

    Wrote the driver to test/execute the Go routines

---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/Peanoquio/goroutines
---
+++ routine_exec.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/Peanoquio/goroutines/routine"
+)
+
+func main() {
+	// NewMyCounter
+	fmt.Println("===== NewMyCounter =====")
+	myCounterObj := routine.NewMyCounter(0)
+	for i := 0; i < 100; i++ {
+		go myCounterObj.Inc()
+	}
+	time.Sleep(time.Second * 1)
+	fmt.Println("final counter value:", myCounterObj.Get())
+
+	// NewMyFibonacci
+	fmt.Println("===== NewMyFibonacci =====")
+	myFibObj := routine.NewMyFibonacci()
+	var fibSeq []int = myFibObj.GenerateFibonacciSeq(10)
+
+	for i, v := range fibSeq {
+		fmt.Printf("index: %d value: %d \n", i, v)
+	}
+
+}
